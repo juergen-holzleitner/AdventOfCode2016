6229677222278a450e5077431e7b20631ff25225
Juergen Holzleitner <juergen@juergen-holzleitner.com>
2025-01-02 17:09:33 +0100

    add 03-Squares_With_Three_Sides

---
+++ 03-Squares_With_Three_Sides/LogicTest.cs
@@ -0,0 +1,13 @@
+﻿using FluentAssertions;
+
+namespace _03_Squares_With_Three_Sides
+{
+  public class LogicTest
+  {
+    [Fact]
+    public void Test1()
+    {
+      1.Should().Be(2);
+    }
+  }
+}
