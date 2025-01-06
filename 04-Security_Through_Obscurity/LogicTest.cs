83dad61fec57281b0890bade4c7653294892125d
Juergen Holzleitner <juergen@juergen-holzleitner.com>
2025-01-06 17:09:14 +0100

    add 04-Security_Through_Obscurity

---
+++ 04-Security_Through_Obscurity/LogicTest.cs
@@ -0,0 +1,13 @@
+﻿using FluentAssertions;
+
+namespace _04_Security_Through_Obscurity
+{
+  public class LogicTest
+  {
+    [Fact]
+    public void Test1()
+    {
+      1.Should().Be(2);
+    }
+  }
+}
