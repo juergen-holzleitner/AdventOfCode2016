2cc166951468b972947cb858d52e3b0bf03cf49c
Juergen Holzleitner <juergen@juergen-holzleitner.com>
2025-01-11 21:46:14 +0100

    add 05-Chess

---
+++ 05-How_About_a_Nice_Game_of_Chess/LogicTest.cs
@@ -0,0 +1,13 @@
+﻿using FluentAssertions;
+
+namespace _05_How_About_a_Nice_Game_of_Chess
+{
+  public class LogicTest
+  {
+    [Fact]
+    public void Test1()
+    {
+      1.Should().Be(2);
+    }
+  }
+}
